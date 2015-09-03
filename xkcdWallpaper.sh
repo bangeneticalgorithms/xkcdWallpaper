8bfd2896367d0d326a15818e889f734d52e254ac
Isuru Daulagala <iud23drexel@gmail.com>
2015-09-03 13:00:42 -0500

    Used xrandr to find number of monitors

--- xkcdWallpaper.sh
+++ xkcdWallpaper.sh
@@ -2,6 +2,9 @@
 
 CURRENT="current.json"
 
+# number of connected monitors
+NUM_MONITORS=$(xrandr -q | grep ' connected' | wc -l)
+echo $NUM_MONITORS
 
 #get the current JSON file
-wget http://xkcd.com/info.0.json -O $CURRENT
+#wget http://xkcd.com/info.0.json -O $CURRENT
