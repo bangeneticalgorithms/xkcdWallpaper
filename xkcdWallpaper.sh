a50bb6236abef43e410439bed3513efef6b8db11
Isuru Daulagala <iud23drexel@gmail.com>
2015-09-03 13:01:42 -0500

    Moved shell commands to global vars

--- xkcdWallpaper.sh
+++ xkcdWallpaper.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-CURRENT="current.json"
+#Common shell commands
+XRANDR=/usr/bin/xrandr
+GREP=/bin/grep
 
+CURRENT="current.json"
 # number of connected monitors
-NUM_MONITORS=$(xrandr -q | grep ' connected' | wc -l)
+NUM_MONITORS=$($XRANDR -q | $GREP ' connected' | wc -l)
 echo $NUM_MONITORS
 
 #get the current JSON file
